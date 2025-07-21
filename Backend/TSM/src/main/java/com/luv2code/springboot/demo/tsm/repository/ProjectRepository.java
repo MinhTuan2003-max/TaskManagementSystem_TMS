50a0fada08a8001d6e354fcc7c3ff924ccdd6561
MINH TUAN <minhtuanha2829@gmail.com>
2025-07-21 18:47:06 +0700

    feat (Project management): Complete and fix bug function for Project manage

--- Backend/TSM/src/main/java/com/luv2code/springboot/demo/tsm/repository/ProjectRepository.java
+++ Backend/TSM/src/main/java/com/luv2code/springboot/demo/tsm/repository/ProjectRepository.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.demo.tsm.repository;
 
 import com.luv2code.springboot.demo.tsm.entity.Project;
+import com.luv2code.springboot.demo.tsm.entity.ProjectMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,10 +14,13 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     List<Project> findByOwnerIdOrderByCreatedAtDesc(Long ownerId);
 
-    @Query("SELECT p FROM Project p JOIN p.members m WHERE m.user.id = :userId")
-    List<Project> findProjectsByMemberId(@Param("userId") Long userId);
+    @Query("SELECT pm.user.id FROM ProjectMember pm WHERE pm.project.id = :projectId")
+    List<Long> findUserIdsByProjectId(@Param("projectId") Long projectId);
 
     @Query("SELECT p FROM Project p WHERE p.owner.id = :userId OR p.id IN " +
             "(SELECT pm.project.id FROM ProjectMember pm WHERE pm.user.id = :userId)")
     List<Project> findAllUserProjects(@Param("userId") Long userId);
+
+    @Query("SELECT pm FROM ProjectMember pm JOIN FETCH pm.user WHERE pm.project.id = :projectId")
+    List<ProjectMember> findByProjectIdWithUser(@Param("projectId") Long projectId);
 }
